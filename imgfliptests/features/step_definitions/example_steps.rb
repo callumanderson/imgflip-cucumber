a75c4edf8269b7f825086a6416b763bf9f39414c
Anderson <callum.anderson@mendeley.com>
2016-01-28 14:25:05 +0000

    Adding some log output to the calls

--- imgfliptests/features/step_definitions/example_steps.rb
+++ imgfliptests/features/step_definitions/example_steps.rb
@@ -1,7 +1,9 @@
 When /^I navigate to google$/ do
+  LOG.info "Navigating to google"
   @browser.goto("https://google.com")
 end
 
 Then /^I should be on the google homepage$/ do
+  LOG.info "Checking browser tite"
   expect(@browser.title).to eq("Google")
 end
